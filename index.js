cf9174157f74e107539b421682ba93286e5a89e2
muhammadirzza <muhammadirzza@gmail.com>
2021-02-18 21:03:21 +0700

    tes

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const express=require('express')
 const bodyParser=require('body-parser')
 const app=express()
-// const cors=require('cors')
-// const bearertoken=require('express-bearer-token')
 
 
 const PORT=5000
-// app.use(cors())//database bisa diakses oleh siapapun
-app.use(bodyParser.json())//buat user kirim data ke server
-app.use(bodyParser.urlencoded({extended:false}))//buat user kirim data ke server
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static('public'))
-// app.use(bearertoken())
 
 const { movieRouters } =require('./routers')
 
 app.get('/',(req, res)=>{
     return res.send('<h1>Belajar MongoDB Node JS</h1>')
 })
-// app.use('/users',userRouters)
-// app.use('/photos',photoRouters)
+
 app.use('/movies',movieRouters)
 
 
